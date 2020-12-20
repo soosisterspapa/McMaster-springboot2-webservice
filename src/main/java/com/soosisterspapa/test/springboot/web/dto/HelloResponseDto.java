c2ee6582f1bb5e2a8488ae1f033e238e0ecfe07a
soosisterspapa <soosisterspapa@gmail.com>
2020-12-20 11:46:12 +0900

    Application, Controller 등록

--- src/main/java/com/soosisterspapa/test/springboot/web/dto/HelloResponseDto.java
+++ src/main/java/com/soosisterspapa/test/springboot/web/dto/HelloResponseDto.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor //
 public class HelloResponseDto {
 
-    private final String name;
-    private final int amount;
+    private final String name ="";
+    private final int amount=0;
 
 }
